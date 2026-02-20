a1ff8f2f89fcf98af86bc76e1ea6f4974042edf4
Kono <anupam.nitrkl@gmail.com>
2026-02-20 15:51:50 +0530

    updates

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,9 +1,24 @@
+import 'dotenv/config'
 import {WebSocketServer} from 'ws';
+import jwt, { JwtPayload } from "jsonwebtoken"
 
 const wss = new WebSocketServer({port: 3000});
 
-wss.on('connection',function connection(ws){
-    ws.on('message',function message(data){
-        ws.send('pong');
-    })
+wss.on('connection',function connection(ws,request){
+    const url = request.url;
+    if(!url){
+        return;
+    }
+    const queryParams = new URLSearchParams(url.split('?')[1])
+    const token = queryParams.get('token');
+    if(!token){
+        ws.close()
+        return;
+    }
+    const decoded = jwt.verify(token,process.env.JWT_SECRET!);
+
+    if(!decoded || !(decoded as JwtPayload).userId){
+        ws.close()
+        return;
+    }
 });
