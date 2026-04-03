79fa4c03f76038583d9b5b724d32ca14954930d5
Kono <anupam.nitrkl@gmail.com>
2026-04-04 04:24:24 +0530

    feat:multiple shapes

--- apps/excalidraw-frontend/app/canvas/[canvasId]/page.tsx
+++ apps/excalidraw-frontend/app/canvas/[canvasId]/page.tsx
@@ -1,17 +1,36 @@
 "use client";
 import { useEffect, useRef } from "react"
 
-export default function CAnvas(){
+export default function Canvas(){
     
     const canvasRef = useRef<HTMLCanvasElement>(null);
+
+    type Shape = {
+        type: "rect",
+        x: number,
+        y: number,
+        width: number,
+        height: number
+    } | {
+        type: "circle",
+        centerX: number,
+        centerY: number,
+        radius: number
+    }
 
     useEffect(()=>{
         if(canvasRef.current){
             const canvas = canvasRef.current;
             const ctx = canvas.getContext("2d")
+         
+            let existingShapes: Shape[] = []
 
             if(!ctx) return;
+            ctx.fillStyle = "black";
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+            ctx.strokeStyle = "white";
 
+            
             let clicked = false
             let startX = 0;
             let startY = 0;
@@ -23,23 +42,48 @@ export default function CAnvas(){
 
             canvas.addEventListener("mouseup", (e) =>{
                 clicked = false
+                const width = e.clientX - startX;
+                const height = e.clientY - startY;
+                existingShapes.push({
+                    type: "rect",
+                    x: startX,
+                    y: startY,
+                    height,
+                    width
+                })
             })
 
             canvas.addEventListener("mousemove", (e) =>{
                 if(clicked){
                     const width = e.clientX - startX;
                     const height = e.clientY - startY;
-                    ctx.clearRect(0,0,canvas.width,canvas.height);
+                    clearCanvas(existingShapes,canvas,ctx)
                     ctx.strokeRect(startX,startY,width,height)
+
                 }
+                
             })
+
+            function clearCanvas(existingShape: Shape[], canvas:HTMLCanvasElement, ctx: CanvasRenderingContext2D){
+
+                ctx.fillStyle = "rgba(0,0,0)"
+                ctx.fillRect(0,0,canvas.width, canvas.height)
+
+                existingShape.map((shape) =>{
+                    if(shape.type === "rect"){
+                    ctx.strokeStyle = "rgba(255,255,255)"   
+                    ctx.strokeRect(shape.x,shape.y,shape.width,shape.height)
+                      
+                    }
+                })
+            }
         
         }
     },[canvasRef])
     
     return(
         <div>
-            <canvas width={500} height={500} ref={canvasRef}></canvas>
+            <canvas width={2000} height={500} ref={canvasRef}></canvas>
         </div>
     )
 }
